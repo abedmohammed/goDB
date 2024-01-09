dba139a8b55a61c6dcb56d3be9ea66d6b8eece3c
rawanmahdi <mahdir3@mcmaster.ca>
2024-01-09 05:47:37 -0500

    Implement helper functions for decoding node data

--- btree/btree.go
+++ btree/btree.go
@@ -1,5 +1,9 @@
 package btree
 
+import (
+	"encoding/binary"
+)
+
 type BNode struct {
 	data []byte // in-memory data, can be dumped to disk
 }
@@ -27,8 +31,101 @@ const BTREE_MAX_KEY_SIZE = 1000
 const BTREE_MAX_VAL_SIZE = 3000
 
 func init() {
-	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE + 100
+	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
 	if node1max > BTREE_PAGE_SIZE {
 		panic("Exceeded page size!")
 	}
+}
+
+// header functions
+// returns node type
+func (node BNode) btype() uint16 {
+	return binary.LittleEndian.Uint16(node.data)
+}
+
+// returns number of keys
+func (node BNode) nkeys() uint16 {
+	return binary.LittleEndian.Uint16(node.data[2:4])
+}
+
+// sets header with first two bytes for node type, second two for number of keys
+func (node BNode) setHeader(btype uint16, nkeys uint16) {
+	binary.LittleEndian.PutUint16(node.data[0:2], btype)
+	binary.LittleEndian.PutUint16(node.data[2:4], nkeys)
+}
+
+// pointer functions
+// returns pointer to child node at given index
+func (node BNode) getPtr(idx uint16) uint64 {
+	if idx >= node.nkeys() {
+		panic("Index out of bounds!")
+	}
+	pos := HEADER + 8*idx
+	return binary.LittleEndian.Uint64(node.data[pos:])
+}
+
+// update child node pointer
+func (node BNode) setPtr(idx uint16, val uint64) {
+	if idx >= node.nkeys() {
+		panic("Index out of bounds!")
+	}
+	pos := HEADER + 8*idx
+	binary.LittleEndian.PutUint64(node.data[pos:], val)
+}
+
+// offset functions
+// returns the value of the offset i.e. the location of the kv-pair at given index
+func offsetPos(node BNode, idx uint16) uint16 {
+	if idx < 1 || idx > node.nkeys() {
+		panic("Index out of bounds!")
+	}
+	return HEADER + 8*node.nkeys() + 2*(idx-1)
+}
+
+// returns offset of kv-pair at given index
+func (node BNode) getOffset(idx uint16) uint16 {
+	if idx == 0 {
+		return 0
+	}
+	return binary.LittleEndian.Uint16(node.data[offsetPos(node, idx):])
+}
+
+// updates offset for kv-pair at given index
+func (node BNode) setOffset(idx uint16, offset uint16) {
+	binary.LittleEndian.PutUint16(node.data[offsetPos(node, idx):], offset)
+}
+
+// key-value pair functions
+// returns position/byte-offset of kv-pair at idx
+func (node BNode) kvPos(idx uint16) uint16 {
+	if idx > node.nkeys() {
+		panic("Index out of bounds!")
+	}
+	return HEADER + 8*node.nkeys() + 2*node.nkeys() + node.getOffset(idx)
+}
+
+// returns key of kv-pair at idx from data array
+func (node BNode) getKey(idx uint16) []byte {
+	if idx >= node.nkeys() {
+		panic("Index out of bounds!")
+	}
+	pos := node.kvPos(idx)                              // byte position of kv-pair
+	klen := binary.LittleEndian.Uint16(node.data[pos:]) // 2 bytes that represent the key length
+	return node.data[pos+4:][:klen]                     // skip klen, vlen, return key
+}
+
+// returns value of kv-pair at idx from data array
+func (node BNode) getVal(idx uint16) []byte {
+	if idx >= node.nkeys() {
+		panic("Index out of bounds!")
+	}
+	pos := node.kvPos(idx) // byte position of kv-pair
+	klen := binary.LittleEndian.Uint16(node.data[pos:])
+	vlen := binary.LittleEndian.Uint16(node.data[pos+2:])
+	return node.data[pos+4+klen:][:vlen]
+}
+
+// return node size in bytes
+func (node BNode) nbytes() uint16 {
+	return node.kvPos(node.nkeys())
 }
